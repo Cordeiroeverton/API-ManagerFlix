57b66b425922ded5ae7a2013cb17eab1f6e84c84
Everton Cordeiro <63669649+Cordeiroeverton@users.noreply.github.com>
2022-01-18 19:02:57 -0300

    inicando

---
+++ src/main/java/io/ordeiroeverton/managerflix/demo/ManagerFlixApplication.java
@@ -0,0 +1,13 @@
+package io.ordeiroeverton.managerflix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ordeiroeverton/managerflix/demo/ManagerFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ordeiroeverton.managerflix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
